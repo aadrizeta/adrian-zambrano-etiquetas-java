9e77fb39010c0b0e0511ec8c001ecfa5f7f372a2
aadrizeta <adrianzambranomontero@gmail.com>
2024-10-17 10:44:18 +0200

    segundo commit

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
-print("hola")
+public class Main {
+	public static void main (String[] args){
+		if (args.length() == 0) {
+			System.ot.println("No hay argumentos");
+		} else {
+			System.ot.println("Hay argumentos");
+		}
+	}
+}
